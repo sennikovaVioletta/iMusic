71a717f72ccb65ce00f7a327003a691a04e2c14d
Violetta Illarionova <illarionovavioletta1@gmail.com>
2022-09-10 19:03:02 +0300

    new cells

--- iMusic/SearchViewController.swift
+++ iMusic/SearchViewController.swift
@@ -12,6 +12,17 @@ class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellid")
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+        cell.textLabel?.text = "indexPath: \(indexPath)"
+        return cell
+        
     }
 }
